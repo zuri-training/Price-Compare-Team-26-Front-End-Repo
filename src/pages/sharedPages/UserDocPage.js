c9762a054cb26634cc3438b31278cc32213bb2e0
olaide-hok <olaidekareem43@yahoo.com>
2022-08-15 10:31:53 +0100

    Setup UserDocPage.js for content addition.
    Use styled components to handle UserDocPage styles.

--- src/pages/sharedPages/UserDocPage.js
+++ src/pages/sharedPages/UserDocPage.js
@@ -1,9 +1,57 @@
 import React from 'react'
+import styled from 'styled-components'
+
 
 const UserDocPage = () => {
   return (
-    <div>UserDocPage</div>
+    <UserDocWrapper>
+      {/* How to Compare Price */}
+      <div>
+
+      </div>
+
+      {/* How to Add to Product */}
+      <div>
+        
+      </div>
+
+      {/* How to get price alerts */}
+      <div>
+        
+      </div>
+
+      {/* How to report an error */}
+      <div>
+        
+      </div>
+
+      {/* How to reset Password */}
+      <div>
+
+      </div>
+
+      {/* How to unsubscribe to Newsletter */}
+      <div>
+
+      </div>
+
+      {/* Text and thumbs div */}
+      <div>
+        
+
+      </div>
+
+
+    </UserDocWrapper>
   )
 }
 
+const UserDocWrapper = styled.div`
+  margin-bottom: 70px;
+  padding: 50px 64px;
+
+  div {
+    margin-bottom: 40px;
+  }
+`
 export default UserDocPage
